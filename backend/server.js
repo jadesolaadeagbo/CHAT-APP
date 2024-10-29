341cd5ce4d80d4533204614b3fec5bad35e0c9ab
Jadesola Adeagbo <jadesolaadeagbo@gmail.com>
2024-10-30 00:02:49 +0100

    Frontend added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv";
+import path from "path"
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.routes.js'
@@ -13,6 +14,9 @@ const PORT = process.env.PORT || 8080;
 dotenv.config();
 
 // Your json middleware should always come before your routes
+
+// app.use(cors({origin: "http://localhost:3000", credentials: true }))
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -20,9 +24,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//     res.send("Hello World")
-// })
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist", "index.html"))
+})
 
 
 app.listen(PORT, () => {
